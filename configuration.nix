a45d2883ba3340cd42a12e572120484e9d180db9
Calmchy <chyrilmanatad@gmail.com>
2026-08-20 22:39:17 +0800

    add a lot of packages

--- configuration.nix
+++ configuration.nix
@@ -84,24 +84,51 @@
   nixpkgs.config.allowUnfree = true;
 
   environment.systemPackages = with pkgs; [
+
+    # web
     wget
+    curl
+    git
+
+    # larp
     fastfetch
     htop
     btop
-    git
+    cmatrix
+    cava
+    nyancat
+    tty-clock
+
+    # files
     zip
     unzip
+    p7zip
+    gparted
     xarchiver
+
+    # XFCE
     xfce4-whiskermenu-plugin
-    brave
-    vscode
-    blueman
-    vim
-    micro
     xfce4-panel-profiles
     xfce4-weather-plugin
+    lm_sensors
+    blueman
     plank
+
+    # text editor
+    vim
+    micro
+    vscode
+
+    # browser
+    brave
+    firefox-esr
+
+    # compilers / interpreter
     jdk8
+    clang
+    python3
+    
+    # container / virtual machine
     docker-compose
     virt-manager
     virt-viewer
@@ -110,6 +137,17 @@
     spice-protocol
     virtio-win   
     win-spice
+
+    # software apps
+    clementine
+    obs-studio
+    handbrake
+    mpv
+
+    # unknown
+    ani-cli
+    syncplay
+    
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
