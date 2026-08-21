5b631828c8f99307f8be0be0a868b32e68566045
Calmchy <chyrilmanatad@gmail.com>
2026-08-22 00:37:11 +0800

    adjust background file path

--- modules/lightdm.nix
+++ modules/lightdm.nix
@@ -3,13 +3,14 @@
 {
   services.xserver.displayManager.lightdm = {
     enable = true;
+
+    background = "/root/background.png"; #"/nix/store/97xdqvza17bp6swl62hh2pw7p6cyrr24-simple-dark-gray-2018-08-28/share/backgrounds/nixos/nix-wallpaper-simple-dark-gray_bottom.png";
+
     greeters.gtk = {
       enable = true;
-
-      background = "/nix/store/97xdqvza17bp6swl62hh2pw7p6cyrr24-simple-dark-gray-2018-08-28/share/backgrounds/nixos/nix-wallpaper-simple-dark-gray_bottom.png";
-
       theme.name = "Adwaita";
       iconTheme.name = "Adwaita";
+      
       cursorTheme = {
         name = "Adwaita";
         size = 16;
