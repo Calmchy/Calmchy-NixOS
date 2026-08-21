31c48be33bf437fbbeef2f0bb780ef1d25f9bb3f
Calmchy <chyrilmanatad@gmail.com>
2026-08-22 00:21:11 +0800

    add lightdm.nix

---
+++ modules/lightdm.nix
@@ -0,0 +1,18 @@
+
+{
+  services.xserver.displayManager.lightdm = {
+    enable = true;
+    greeters.gtk = {
+      enable = true;
+
+      background = /nix/store/97xdqvza17bp6swl62hh2pw7p6cyrr24-simple-dark-gray-2018-08-28/share/backgrounds/nixos/nix-wallpaper-simple-dark-gray_bottom.png;
+
+      theme.name = "Adwaita";
+      iconTheme.name = "Adwaita";
+      cursorTheme = {
+        name = "Adwaita";
+        size = 16;
+      };
+    };
+  };
+}
