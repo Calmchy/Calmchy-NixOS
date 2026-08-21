6df0ca5a7d1c18f776c538db58c5e4b069a8c38a
Calmchy <chyrilmanatad@gmail.com>
2026-08-21 23:50:08 +0800

    remove picom on configuration.nix and add import for picom.nix

--- configuration.nix
+++ configuration.nix
@@ -6,6 +6,7 @@
       ./packages.nix
       ./virtualisation.nix
       ./php.nix
+      ./picom.nix
     ];
 
   # boot.loader.grub.enable = true;
@@ -60,47 +61,6 @@
   services.xserver.xkb = {
     layout = "us";
     variant = "";
-  };
-
-  services.picom = {
-    enable = true;
-    backend = "glx";
-    vSync = true;
-    # fade = false;
-    # fadeDelta = 4;
-    shadow = true;
-    shadowOpacity = 0.6;
-    settings = {
-      corner-radius = 8;
-
-      shadow-exclude = [
-        "class_g = 'Plank'"
-        "window_type = 'dock'"
-        "window_type = 'desktop'"
-        "_GTK_FRAME_EXTENTS@:c"
-        "window_type = 'menu'"
-        "window_type = 'popup_menu'"
-        "window_type = 'dropdown_menu'"
-      ];
-
-      corner-radius-rules = [
-        "0:class_g = 'Plank'"
-      ];
-
-      blur-method = "dual_kawase";
-      blur-strength = 5;
-      # blur-background-frame = false;
-      blur-background-fixed = true;
-
-      blur-background-exclude = [
-        "class_g = 'Plank'"
-        "_GTK_FRAME_EXTENTS@:c"
-        "class_g = 'Xfce4-screenshooter'"
-        "class_i = 'xfce4-screenshooter'"
-      ];
-
-       unredir-if-possible = true;
-    };
   };
 
   # Enable CUPS to print documents.
