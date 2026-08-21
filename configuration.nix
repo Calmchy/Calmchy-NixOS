3e002bf9977bd9e914cc7e1d0661dc34e5ce6c54
Calmchy <chyrilmanatad@gmail.com>
2026-08-21 23:47:04 +0800

    fix duplicate and minor config fix

--- configuration.nix
+++ configuration.nix
@@ -3,6 +3,7 @@
 {
   imports =
     [
+      ./packages.nix
       ./virtualisation.nix
       ./php.nix
     ];
@@ -65,8 +66,8 @@
     enable = true;
     backend = "glx";
     vSync = true;
-    fade = true;
-    fadeDelta = 4;
+    # fade = false;
+    # fadeDelta = 4;
     shadow = true;
     shadowOpacity = 0.6;
     settings = {
@@ -88,8 +89,7 @@
 
       blur-method = "dual_kawase";
       blur-strength = 5;
-      #blur-background-frame = false;
-
+      # blur-background-frame = false;
       blur-background-fixed = true;
 
       blur-background-exclude = [
@@ -134,78 +134,6 @@
       thunderbird
     ];
   };
-
-  nixpkgs.config.allowUnfree = true;
-
-  environment.systemPackages = with pkgs; [
-
-    # web
-    wget
-    curl
-    git
-
-    # larp
-    fastfetch
-    htop
-    btop
-    cmatrix
-    cava
-    nyancat
-    tty-clock
-
-    # files
-    zip
-    unzip
-    p7zip
-    gparted
-    xarchiver
-
-    # XFCE
-    xfce4-whiskermenu-plugin
-    xfce4-panel-profiles
-    xfce4-weather-plugin
-    lm_sensors
-    blueman
-    plank
-
-    # text editor
-    vim
-    micro
-    vscode
-
-    # browser
-    brave
-    firefox-esr
-
-    # compilers / interpreter
-    jdk8
-    clang
-    python3
-    
-    # container / virtual machine
-    docker-compose
-    virt-manager
-    virt-viewer
-    spice
-    spice-gtk
-    spice-protocol
-    virtio-win   
-    win-spice
-
-    # software apps
-    clementine
-    obs-studio
-    handbrake
-    tigervnc
-    veracrypt
-    mpv
-
-    # unknown
-    ani-cli
-    syncplay
-    pciutils
-    mesa-demos
-  ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
