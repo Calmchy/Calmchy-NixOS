40e39c814f604c7a94da62a16574de4ee6b0b4a1
Calmchy <chyrilmanatad@gmail.com>
2026-08-23 14:57:15 +0800

    link to unstable version

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS config for calmchy";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs = { self, nixpkgs, ... }: {
