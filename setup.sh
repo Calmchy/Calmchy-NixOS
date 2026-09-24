133e7764bb775ddc6aee6665602ecb90c9a45796
Calmchy <192443316+Calmchy@users.noreply.github.com>
2026-09-24 14:05:48 +0800

    Refactor setup script for user creation and installation

--- setup.sh
+++ setup.sh
@@ -1,29 +1,34 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
 
 # create user
 read -p "Enter your fullname: " fullname
 read -p "Enter your username: " username
-echo "{ config, pkgs, ... }:
+
+cat > hosts/user.nix << EOF
+{ config, pkgs, ... }:
 
 {
   users.users.$username = {
     isNormalUser = true;
     description = "$fullname";
-    shell = pkgs.fish; 
+    shell = pkgs.fish;
     extraGroups = [ "networkmanager" "wheel" "docker" "libvirtd" ];
     packages = with pkgs; [
-    thunderbird
+      thunderbird
     ];
   };
 }
-" >> hosts/user.nix
+EOF
 
 # Generate hardware-configuration
 nixos-generate-config --root /mnt --show-hardware-config > /mnt/etc/nixos/hosts/hardware-configuration.nix
 
-NIX_CONFIG = "experimental-features = nix=command flakes" nixos-install --flake /mnt/etc/nixos#nixos
+# Install
+export NIX_CONFIG="experimental-features = nix-command flakes"
+nixos-install --flake /mnt/etc/nixos#nixos
 
 echo "Set $username a password!"
-nixos-enter --root /mnt -c 'passwd $username'
+nixos-enter --root /mnt -c "passwd $username"
 
 reboot
