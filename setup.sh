60a1c59748ff7d0c401ae6986c01591fe175e670
Calmchy <chyrilmanatad@gmail.com>
2026-09-24 18:36:17 +0800

    update

--- setup.sh
+++ setup.sh
@@ -1,34 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# create user
-read -p "Enter your fullname: " fullname
-read -p "Enter your username: " username
-
-cat > hosts/user.nix << EOF
-{ config, pkgs, ... }:
-
-{
-  users.users.$username = {
-    isNormalUser = true;
-    description = "$fullname";
-    shell = pkgs.fish;
-    extraGroups = [ "networkmanager" "wheel" "docker" "libvirtd" ];
-    packages = with pkgs; [
-      thunderbird
-    ];
-  };
-}
-EOF
-
-# Generate hardware-configuration
-nixos-generate-config --root /mnt --show-hardware-config > /mnt/etc/nixos/hosts/hardware-configuration.nix
 
 # Install
 export NIX_CONFIG="experimental-features = nix-command flakes"
-nixos-install --flake /mnt/etc/nixos#nixos
+sudo nixos-rebuild switch --flake .
 
-echo "Set $username a password!"
-nixos-enter --root /mnt -c "passwd $username"
-
-reboot
+echo "You may now reboot"
